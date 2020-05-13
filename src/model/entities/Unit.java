a9b640e0d7e30d12ecaa18111db94aa376c6854b
Pedro Christofoletti <pedrofernandoch@gmail.com>
2020-05-13 19:27:30 -0300

    Creating constructor, getter and setter

--- src/model/entities/Unit.java
+++ src/model/entities/Unit.java
@@ -2,4 +2,17 @@ package model.entities;
 
 public class Unit {
 
+	private String name;
+
+	public Unit(String name) {
+		setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
