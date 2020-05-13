baabeda0dcb0bf4d2c5cb544b42c651a42e9b61d
Pedro Christofoletti <pedrofernandoch@gmail.com>
2020-05-13 19:29:01 -0300

    Creating constructor, getter and setter

--- src/model/entities/Category.java
+++ src/model/entities/Category.java
@@ -2,4 +2,17 @@ package model.entities;
 
 public class Category {
 
+	private String name;
+
+	public Category(String name) {
+		setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
