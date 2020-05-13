be94bc13407cd2238582c91a0cd96fef1c9ae531
Pedro Christofoletti <pedrofernandoch@gmail.com>
2020-05-13 19:40:46 -0300

    Creating constructor, getters and setters

--- src/model/entities/RecipeIngredient.java
+++ src/model/entities/RecipeIngredient.java
@@ -1,5 +1,39 @@
 package model.entities;
 
 public class RecipeIngredient {
+	
+	private Ingredient ingredient;
+	private float amount;
+	private Unit unit;
+	
+	public RecipeIngredient(Ingredient ingredient, float amount, Unit unit) {
+		setIngredient(ingredient);
+		setAmount(amount);
+		setUnit(unit);
+	}
+
+	public Ingredient getIngredient() {
+		return ingredient;
+	}
+
+	public void setIngredient(Ingredient ingredient) {
+		this.ingredient = ingredient;
+	}
+
+	public float getAmount() {
+		return amount;
+	}
+
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+
+	public Unit getUnit() {
+		return unit;
+	}
+
+	public void setUnit(Unit unit) {
+		this.unit = unit;
+	}
 
 }
