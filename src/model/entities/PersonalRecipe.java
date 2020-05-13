bff7695a64ec33d3b07fe534dd687874c0d6bf54
Pedro Christofoletti <pedrofernandoch@gmail.com>
2020-05-13 19:57:25 -0300

    Creating constructor, getters, setters and toString

--- src/model/entities/PersonalRecipe.java
+++ src/model/entities/PersonalRecipe.java
@@ -1,5 +1,13 @@
 package model.entities;
 
+import java.sql.Time;
+import java.util.ArrayList;
+
 public class PersonalRecipe extends Recipe{
+
+	public PersonalRecipe(String name, ArrayList<Category> categories, String directions, Time preparationTime,
+			Time cookTime, int servings) {
+		super(name, categories, directions, preparationTime, cookTime, servings);
+	}
 
 }
--- src/model/entities/Recipe.java
+++ src/model/entities/Recipe.java
@@ -1,5 +1,79 @@
 package model.entities;
 
+import java.sql.Time;
+import java.util.ArrayList;
+
 public class Recipe {
+
+	private String name;
+	private ArrayList<Category> categories;
+	private String directions;
+	private Time preparationTime;
+	private Time cookTime;
+	private int servings;
+
+	public Recipe(String name, ArrayList<Category> categories, String directions, Time preparationTime, Time cookTime,
+			int servings) {
+		this.name = name;
+		this.categories = categories;
+		this.directions = directions;
+		this.preparationTime = preparationTime;
+		this.cookTime = cookTime;
+		this.servings = servings;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(ArrayList<Category> categories) {
+		this.categories = categories;
+	}
+
+	public String getDirections() {
+		return directions;
+	}
+
+	public void setDirections(String directions) {
+		this.directions = directions;
+	}
+
+	public Time getPreparationTime() {
+		return preparationTime;
+	}
+
+	public void setPreparationTime(Time preparationTime) {
+		this.preparationTime = preparationTime;
+	}
+
+	public Time getCookTime() {
+		return cookTime;
+	}
+
+	public void setCookTime(Time cookTime) {
+		this.cookTime = cookTime;
+	}
+
+	public int getServings() {
+		return servings;
+	}
+
+	public void setServings(int servings) {
+		this.servings = servings;
+	}
+
+	@Override
+	public String toString() {
+		return "Recipe [name=" + name + ", categories=" + categories + ", directions=" + directions
+				+ ", preparationTime=" + preparationTime + ", cookTime=" + cookTime + ", servings=" + servings + "]";
+	}
 
 }
