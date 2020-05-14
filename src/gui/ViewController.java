64389458d0b80594117a43688887c50ea8570955
Pedro Christofoletti <pedrofernandoch@gmail.com>
2020-05-14 18:40:24 -0300

    Creating package gui with view and view controller

---
+++ src/gui/ViewController.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ViewController {
+	
+	@FXML
+	private Button findRecipes;
+	@FXML
+	public void onFindRecipesAction() {
+		System.out.println("RECEITAS:");
+	}
+
+}
