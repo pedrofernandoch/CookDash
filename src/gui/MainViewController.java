1626f7b5f0484be77fc01db12b03f2053277e6b7
Pedro Christofoletti <pedrofernandoch@gmail.com>
2020-05-15 18:46:32 -0300

    Fixing file name

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -12,15 +12,34 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 
 public class MainViewController {
-	
+
 	@FXML
 	private Button findRecipes;
+
 	@FXML
 	public void onFindRecipesAction() {
 		Alerts.showAlert("Alert title", "Alert header", "Alert content", AlertType.INFORMATION);
 	}
-	
+
 	@FXML
 	private Button showRecipes;
+
+	@FXML
+	public void onShowRecipesAction() {
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			conn = SQLiteConnection.getConnection();
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT * FROM recipes");
+
+			while (rs.next()) {
+				System.out.println(rs.getString("name") + ", " + rs.getString("directions"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
