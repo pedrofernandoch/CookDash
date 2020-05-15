edf7689bb36e564a19cb0e7b24ffa93aa37b04d4
Pedro Christofoletti <pedrofernandoch@gmail.com>
2020-05-15 16:32:58 -0300

    Creating db package and SQLiteConnection

---
+++ src/db/SQLiteConnection.java
@@ -0,0 +1,24 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLiteConnection {
+	
+	private static final String conn = "jdbc:sqlite:CookDash.sqlite";
+	
+	public static Connection getConnection() throws SQLException{
+		try {
+			Class.forName("org.sqlite.JDBC");
+			return DriverManager.getConnection(conn);
+		}
+		catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+		
+
+}
