f68bede5c56b2a8e2bd0e83e4ecc419101e4eb25
Pedro Christofoletti <pedrofernandoch@gmail.com>
2020-05-19 12:53:34 -0300

    Adding abstract class: Item to model.entities

---
+++ src/model/entities/Item.java
@@ -0,0 +1,18 @@
+package model.entities;
+
+public abstract class Item {
+	
+	private String name;
+
+	public Item(String name) {
+		setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
