30717c5cb31d3b99218f9a703f44932a693c8a3c
Daniel <daniel.m.fucci@gmail.com>
2020-05-22 21:56:51 -0300

    Creating DB structure

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -44,6 +44,75 @@ public class MainViewController implements Initializable{
 			e.printStackTrace();
 		}
 	}
+	
+	@FXML
+	public void createDB() {
+		Connection conn = null;
+		Statement st = null;
+		try {
+			conn = SQLiteConnection.getConnection();
+			st = conn.createStatement();
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS Recipes (\n"
+	                + "	id integer PRIMARY KEY,\n"
+	                + "	name text NOT NULL,\n"
+	                + "	directions text NOT NULL,\n"
+	                + "	preparationTime real,\n"
+	                + "	servings integer\n"
+	                + ");");
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS Categories (\n"
+	                + "	id integer PRIMARY KEY,\n"
+	                + "	name text NOT NULL\n"
+	                + ");");
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS Recipe_Categories (\n"
+	                + "	id integer PRIMARY KEY,\n"
+	                + "	FOREIGN KEY (id)\n" 
+	                + " REFERENCES Recipes (recipe_id),\n"
+	                + "	FOREIGN KEY (id)\n" 
+	                + " REFERENCES Categories (category_id)\n"
+	                + ");");
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS Ingredients (\n"
+	                + "	id integer PRIMARY KEY,\n"
+	                + "	name text NOT NULL\n"
+	                + ");");
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS Units (\n"
+	                + "	id integer PRIMARY KEY,\n"
+	                + "	name text NOT NULL\n"
+	                + ");");
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS Recipe_Ingredients (\n"
+	                + "	id integer PRIMARY KEY,\n"
+	                + "	FOREIGN KEY (id)\n" 
+	                + " REFERENCES Recipes (recipe_id),\n"
+	                + "	FOREIGN KEY (id)\n" 
+	                + " REFERENCES Ingredients (ingredient_id)\n"
+	                + ");");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	@FXML
+	public void showAllTables() {
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			conn = SQLiteConnection.getConnection();
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT \n" + 
+					"    name\n" + 
+					"FROM \n" + 
+					"    sqlite_master \n" + 
+					"WHERE \n" + 
+					"    type ='table' AND \n" + 
+					"    name NOT LIKE 'sqlite_%';");
+
+			while (rs.next()) {
+				System.out.println(rs.getString("name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
