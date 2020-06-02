227e97f68ae1c7365fb9d21067100bfd29747143
Pedro Christofoletti <pedrofernandoch@gmail.com>
2020-06-02 18:29:07 -0300

    Moving db functions to SQLiteConnection class

--- src/db/SQLiteConnection.java
+++ src/db/SQLiteConnection.java
@@ -2,7 +2,9 @@ package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SQLiteConnection {
 
@@ -11,12 +13,60 @@ public class SQLiteConnection {
 	public static Connection getConnection() throws SQLException {
 		try {
 			Class.forName("org.sqlite.JDBC");
+			initialize();
 			return DriverManager.getConnection(conn);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 
 		return null;
+	}
+
+	private static void initialize() {
+		Connection conn = null;
+		Statement st = null;
+		try {
+			conn = getConnection();
+			st = conn.createStatement();
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS Recipes (\n" + "	id integer PRIMARY KEY,\n"
+					+ "	name text NOT NULL,\n" + "	directions text NOT NULL,\n" + "	preparationTime real,\n"
+					+ "	servings integer\n" + ");");
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS MyRecipes (\n" + "	id integer PRIMARY KEY,\n"
+					+ "	name text NOT NULL,\n" + "	directions text NOT NULL,\n" + "	preparationTime real,\n"
+					+ "	servings integer\n" + ");");
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS Categories (\n" + "	id integer PRIMARY KEY,\n"
+					+ "	name text NOT NULL\n" + ");");
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS Recipe_Categories (\n" + "	id integer PRIMARY KEY,\n"
+					+ "	FOREIGN KEY (id)\n" + " REFERENCES Recipes (recipe_id),\n" + "	FOREIGN KEY (id)\n"
+					+ " REFERENCES Categories (category_id)\n" + ");");
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS Ingredients (\n" + "	id integer PRIMARY KEY,\n"
+					+ "	name text NOT NULL\n" + ");");
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS Units (\n" + "	id integer PRIMARY KEY,\n"
+					+ "	name text NOT NULL\n" + ");");
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS Recipe_Ingredients (\n" + "	id integer PRIMARY KEY,\n"
+					+ "	FOREIGN KEY (id)\n" + " REFERENCES Recipes (recipe_id),\n" + "	FOREIGN KEY (id)\n"
+					+ " REFERENCES Ingredients (ingredient_id)\n" + ");");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void showAllTables() {
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			conn = SQLiteConnection.getConnection();
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT \n" + "    name\n" + "FROM \n" + "    sqlite_master \n" + "WHERE \n"
+					+ "    type ='table' AND \n" + "    name NOT LIKE 'sqlite_%';");
+
+			while (rs.next()) {
+				System.out.println(rs.getString("name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,20 +1,16 @@
 package gui;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 public class MainViewController {
 	
@@ -71,54 +67,3 @@ public class MainViewController {
 	}
 
 }
-//Alerts.showAlert("Alert title", "Alert header", "Alert content",
-	// AlertType.INFORMATION);
-
-	//COLOCAR NA MAIN OU NO CONNECTOR DO DB
-	/*public void createDB() {
-		Connection conn = null;
-		Statement st = null;
-		try {
-			conn = SQLiteConnection.getConnection();
-			st = conn.createStatement();
-			st.executeUpdate("CREATE TABLE IF NOT EXISTS Recipes (\n" + "	id integer PRIMARY KEY,\n"
-					+ "	name text NOT NULL,\n" + "	directions text NOT NULL,\n" + "	preparationTime real,\n"
-					+ "	servings integer\n" + ");");
-			st.executeUpdate("CREATE TABLE IF NOT EXISTS Categories (\n" + "	id integer PRIMARY KEY,\n"
-					+ "	name text NOT NULL\n" + ");");
-			st.executeUpdate("CREATE TABLE IF NOT EXISTS Recipe_Categories (\n" + "	id integer PRIMARY KEY,\n"
-					+ "	FOREIGN KEY (id)\n" + " REFERENCES Recipes (recipe_id),\n" + "	FOREIGN KEY (id)\n"
-					+ " REFERENCES Categories (category_id)\n" + ");");
-			st.executeUpdate("CREATE TABLE IF NOT EXISTS Ingredients (\n" + "	id integer PRIMARY KEY,\n"
-					+ "	name text NOT NULL\n" + ");");
-			st.executeUpdate("CREATE TABLE IF NOT EXISTS Units (\n" + "	id integer PRIMARY KEY,\n"
-					+ "	name text NOT NULL\n" + ");");
-			st.executeUpdate("CREATE TABLE IF NOT EXISTS Recipe_Ingredients (\n" + "	id integer PRIMARY KEY,\n"
-					+ "	FOREIGN KEY (id)\n" + " REFERENCES Recipes (recipe_id),\n" + "	FOREIGN KEY (id)\n"
-					+ " REFERENCES Ingredients (ingredient_id)\n" + ");");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@FXML
-	private Button showAllTables;
-
-	@FXML
-	public void onshowAllTablesAction() {
-		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
-		try {
-			conn = SQLiteConnection.getConnection();
-			st = conn.createStatement();
-			rs = st.executeQuery("SELECT \n" + "    name\n" + "FROM \n" + "    sqlite_master \n" + "WHERE \n"
-					+ "    type ='table' AND \n" + "    name NOT LIKE 'sqlite_%';");
-
-			while (rs.next()) {
-				System.out.println(rs.getString("name"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}*/
