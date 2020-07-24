8b45124344581914bdbda945bf792ecc0c652662
Pedro Christofoletti <pedrofernandoch@gmail.com>
2020-07-24 20:06:28 -0300

    Addind favorites and myrecipes

--- src/gui/MyRecipesViewController.java
+++ src/gui/MyRecipesViewController.java
@@ -1,24 +1,81 @@
 package gui;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import model.entities.PersonalRecipe;
 
-public class MyRecipesViewController {
+public class MyRecipesViewController implements Initializable {
+
+	//private static Node table;
 	
 	@FXML
-    private TableView<?> tableView;
-
-    @FXML
-    private TableColumn<?, ?> tcRecipe;
-
-    @FXML
-    private TableColumn<?, ?> tcView;
-
-    @FXML
-    private TableColumn<?, ?> tcEdit;
-
-    @FXML
-    private TableColumn<?, ?> tcDelet;
+	private Button back;
 	
+    @FXML
+    private TableView<PersonalRecipe> tableView;
+
+    @FXML
+    private TableColumn<PersonalRecipe, String> tcRecipe;
+
+    @FXML
+    private TableColumn<PersonalRecipe, Button> tcView;
+
+    @FXML
+    private TableColumn<PersonalRecipe, Button> tcEdit;
+
+    @FXML
+    private TableColumn<PersonalRecipe, Button> tcDelet;
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		//initializeNodes();
+		back.setDisable(true);
+	}
+	
+	/*public static void loadRecipe() {
+		try {
+			FXMLLoader loader = new FXMLLoader(RecipeFoundViewController.class.getResource("/gui/RecipeView.fxml"));
+			VBox newVBox = loader.load();
+			//CHAMADA DO BANCO
+			Scene mainScene = SearchRecipeViewController.getRecipeFound();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			Node pane = mainVBox.getChildren().get(0);
+			Node bt = ((Pane)pane).getChildren().get(1);
+			((Button)bt).setDisable(false);
+			table = mainVBox.getChildren().get(1);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(pane);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+		}
+		catch (IOException exc) {
+			Alerts.showAlert("IO Exception", "Error loading view", exc.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	public void onBacksAction() {
+		//CHAMADA DO BANCO
+		Scene mainScene = SearchRecipeViewController.getRecipeFound();
+		VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+		Node pane = mainVBox.getChildren().get(0);
+		mainVBox.getChildren().clear();
+		mainVBox.getChildren().add(pane);
+		mainVBox.getChildren().add(table);
+		back.setDisable(true);
+	}
+
+	private void initializeNodes() {
+		//CHAMADA DO BANCO
+		ObservableList<PersonalRecipe> obsList;
+		//tableView.setItems(obsList);
+		tcRecipe.setCellValueFactory(new PropertyValueFactory<>("name"));
+		tcEdit.setCellValueFactory(new PropertyValueFactory<>("edit"));	
+		tcDelet.setCellValueFactory(new PropertyValueFactory<>("delet"));	
+		tcView.setCellValueFactory(new PropertyValueFactory<>("view"));
+	}*/
 }
--- src/model/entities/PersonalRecipe.java
+++ src/model/entities/PersonalRecipe.java
@@ -1,9 +1,63 @@
 package model.entities;
 
-public class PersonalRecipe extends Recipe {
+import javafx.scene.control.Button;
 
-	public PersonalRecipe(Recipe recipe) {
+public class PersonalRecipe extends Recipe {
+	
+	public Button view;
+	public Button edit;
+	public Button delet;
+
+	public PersonalRecipe(Recipe recipe, Button view, Button edit, Button delet) {
 		super(recipe.getView(),recipe.getName(), recipe.getCategories(), recipe.getIngredients(), recipe.getDirections(), recipe.getPreparationTime(), recipe.getMatch(),recipe.getServings(), recipe.getIndex());
+		setView(view);
+		setEdit(edit);
+		setDelet(delet);
+		view.setOnAction(e ->{
+			//RecipeViewController.setIndex(index);
+			//RecipeFoundViewController.loadRecipe();
+		});
+
+
+		edit.setOnAction(e ->{
+			//RecipeViewController.setIndex(index);
+			//RecipeFoundViewController.loadRecipe();
+			//addIngredient();
+			//removeIngredient();
+			//addCategory();
+			//removeCategory();	
+			//editDirections();
+		});
+
+
+		delet.setOnAction(e ->{
+			//RecipeViewController.setIndex(index);
+			//RecipeFoundViewController.loadRecipe();
+		});
+	}
+	
+	public Button getView() {
+		return view;
+	}
+
+	public void setView(Button view) {
+		this.view = view;
+	}
+
+	public Button getEdit() {
+		return edit;
+	}
+
+	public void setEdit(Button edit) {
+		this.edit = edit;
+	}
+
+	public Button getDelet() {
+		return delet;
+	}
+
+	public void setDelet(Button delet) {
+		this.delet = delet;
 	}
 
 	public void addIngredient(Ingredient ingredient, float amount, Unit unit) {
